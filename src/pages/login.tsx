e062084bd6533e4702a174ab454e690ea693918a
Aekkawan Klapsan <aekkawan.k@apthai.com>
2024-07-03 23:19:38 +0700

    fix: cosmetic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,7 @@ import LoginLayout from '@/components/Layouts/LoginLayout';
 const LoginPage: React.FC = () => {
   return (
     <>
-      <h1 className="text-center text-3xl font-bold text-blue-600">Todo App Login</h1>
+      <h1 className="text-center text-3xl font-bold text-blue-500 uppercase">Todo App Login</h1>
       <div className="max-w-md w-full bg-white p-8 rounded-lg mt-4 m-auto shadow-md">
         <AuthForm />
       </div>
